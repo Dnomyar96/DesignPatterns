355bca9beb1f87c5235c503416721a3d468c2706
Raymond Benjamins <raymond.benjamins@telfort.nl>
2018-02-20 16:10:32 +0100

    Initial + Observer pattern

---
+++ DesignPatterns/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WeatherData _weatherData;
+        private Observer _observer;
+
+        public MainWindow()
+        {
+            _weatherData = new WeatherData();
+            _observer = new Observer(_weatherData, this);
+            InitializeComponent();
+            _weatherData.SetMeasurements(15.5f, 70f, 20f);
+        }
+
+        private void observerButton_Click(object sender, RoutedEventArgs e)
+        {
+            var random = new Random();
+            _weatherData.SetMeasurements(random.Next(0, 30), random.Next(50, 100), random.Next(0, 100));
+        }
+
+        public void SetObserverLabel(float temp, float humidity, float pressure)
+        {
+            observerLabel.Content = $"Temperature: {temp}; Humidity: {humidity}; Pressure: {pressure};";
+        }
+    }
+}
---
+++ DesignPatterns/Observer.cs
@@ -0,0 +1,38 @@
+﻿using Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class Observer : IObserver, IDisplayElement
+    {
+        private float _temperature;
+        private float _humidity;
+        private float _pressure;
+        private ISubject _weatherData;
+        private MainWindow _window;
+
+        public Observer(ISubject weatherData, MainWindow window)
+        {
+            _weatherData = weatherData;
+            _window = window;
+            weatherData.RegisterObserver(this);
+        }
+
+        public void Display()
+        {
+            _window.SetObserverLabel(_temperature, _humidity, _pressure);
+        }
+
+        public void Update(float temp, float humidity, float pressure)
+        {
+            _temperature = temp;
+            _humidity = humidity;
+            _pressure = pressure;
+            Display();
+        }
+    }
+}
